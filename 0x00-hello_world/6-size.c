1668d5ed37688f2495896780ec0735965f0b9fef
Ebnen <Ebenzy1996@gmail.com>
2023-08-14 17:42:49 -0700

    nhdnf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("Size of char: %lu bytes\n", sizeof(char));
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("Size of long int: %lu bytes\n", sizeof(long int));
+printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+printf("Size of float: %lu bytes\n", sizeof(float));
+return (0);
+}
+~                                                                                                                                                    
+~                 
